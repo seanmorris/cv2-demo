af3a11389f56d0f8f88237e854f54a89084255c1
Sean Morris <sean@seanmorr.is>
2020-11-28 03:11:59 -0500

    Multiplayer gamepad

--- app/demos/gamepad/GamepadDemo.js
+++ app/demos/gamepad/GamepadDemo.js
@@ -1,47 +1,29 @@
 import { View } from 'curvature/base/View';
 
+import { GamepadView } from './GamepadView';
+
 export class GamepadDemo extends View
 {
+	template = require('./template.html');
 
 	constructor(args, parent)
 	{
 		super(args, parent);
 
-		this.template = require('./template.html');
-
 		this.listen(window, 'gamepadconnected', event => this.padConnected(event))
 
 		this.args.pads = [];
-
-		this.args.lx = 0;
-		this.args.ly = 0;
-		this.args.rx = 0;
-		this.args.ry = 0;
-
-		this.args.b0 = 0;
-		this.args.b1 = 0;
-		this.args.b2 = 0;
-		this.args.b3 = 0;
-		this.args.b4 = 0;
-		this.args.b5 = 0;
-		this.args.b6 = 0;
-		this.args.b7 = 0;
-		this.args.b8 = 0;
-		this.args.b9 = 0;
-		this.args.b10 = 0;
-		this.args.b11 = 0;
-		this.args.b12 = 0;
-		this.args.b13 = 0;
-		this.args.b14 = 0;
-		this.args.b15 = 0;
-		this.args.b16 = 0;
 	}
 
 	padConnected(event)
 	{
-		const pad = event.gamepad;
+		const pad  = event.gamepad;
 
-		this.args.pads[ pad.index ] = pad;
+		const view = new GamepadView({pad}, this);
+
+		this.args.pads[ pad.index ] = view;
+
+		console.log(this.args.pads);
 
 		const l = ()=> { this.loop(); requestAnimationFrame(l); };
 
@@ -51,28 +33,54 @@ export class GamepadDemo extends View
 	loop()
 	{
 		const pads = navigator.getGamepads();
+		const del  = [];
 
 		for(let i = 0; i < pads.length; i++)
 		{
-			const pad = pads[i];
-
-			if(!pad)
+			if(!this.args.pads[i])
 			{
+				this.args.pads[i] = null;
 				continue;
 			}
 
-			this.args.lx = pad.axes[0];
-			this.args.ly = pad.axes[1];
-			this.args.rx = pad.axes[2];
-			this.args.ry = pad.axes[3];
-
-			for(let ii = 0; ii < pad.buttons.length; ii++)
+			if(!pads[i])
 			{
-				const button = pad.buttons[ii];
-
-				this.args['b' + ii] = button.value;
+				del.push(i);
+				continue;
 			}
+
+			const padView = this.args.pads[i];
+
+			padView.onLoop(pads[i]);
 		}
+
+		for(let i in del.reverse())
+		{
+			// delete this.args.pads[ del[i] ];
+		}
+
+		// {
+		// 	const pad = pads[i];
+
+		// 	if(!pad)
+		// 	{
+		// 		continue;
+		// 	}
+
+		// 	this.args.lx = pad.axes[0];
+		// 	this.args.ly = pad.axes[1];
+		// 	this.args.rx = pad.axes[2];
+		// 	this.args.ry = pad.axes[3];
+
+		// 	for(let ii = 0; ii < pad.buttons.length; ii++)
+		// 	{
+		// 		const button = pad.buttons[ii];
+
+		// 		this.args['b' + ii] = button.value > this.args['b' + ii]
+		// 			? button.value
+		// 			: this.args['b' + ii];
+		// 	}
+		// }
 	}
 }
 
---
+++ app/demos/gamepad/GamepadView.js
@@ -0,0 +1,72 @@
+import { View } from 'curvature/base/View';
+
+export class GamepadView extends View
+{
+	template = require('./gamepad.html');
+
+	constructor(args, parent)
+	{
+		super(args, parent);
+
+		this.args.lx = 0;
+		this.args.ly = 0;
+		this.args.rx = 0;
+		this.args.ry = 0;
+
+		this.args.b0 = 0;
+		this.args.b1 = 0;
+		this.args.b2 = 0;
+		this.args.b3 = 0;
+		this.args.b4 = 0;
+		this.args.b5 = 0;
+		this.args.b6 = 0;
+		this.args.b7 = 0;
+		this.args.b8 = 0;
+		this.args.b9 = 0;
+		this.args.b10 = 0;
+		this.args.b11 = 0;
+		this.args.b12 = 0;
+		this.args.b13 = 0;
+		this.args.b14 = 0;
+		this.args.b15 = 0;
+		this.args.b16 = 0;
+	}
+
+	onLoop(pad)
+	{
+		const minDead = 0.20;
+
+		this.args.lx = Number(pad.axes[0]);
+		this.args.ly = Number(pad.axes[1]);
+		this.args.rx = Number(pad.axes[2]);
+		this.args.ry = Number(pad.axes[3]);
+
+		this.args.lxAdjust = (Math.abs(pad.axes[0]) > minDead ? pad.axes[0] : 0).toFixed(2);
+		this.args.lyAdjust = (Math.abs(pad.axes[1]) > minDead ? pad.axes[1] : 0).toFixed(2);
+		this.args.rxAdjust = (Math.abs(pad.axes[2]) > minDead ? pad.axes[2] : 0).toFixed(2);
+		this.args.ryAdjust = (Math.abs(pad.axes[3]) > minDead ? pad.axes[3] : 0).toFixed(2);
+
+		const lMag = Math.sqrt(this.args.lxAdjust**2 + this.args.lyAdjust**2);
+
+		if(lMag > 1)
+		{
+			this.args.lxAdjust /= lMag;
+			this.args.lyAdjust /= lMag;
+		}
+
+		const rMag = Math.sqrt(this.args.rxAdjust**2 + this.args.ryAdjust**2);
+
+		if(rMag > 1)
+		{
+			this.args.rxAdjust /= rMag;
+			this.args.ryAdjust /= rMag;
+		}
+
+		for(let ii = 0; ii < pad.buttons.length; ii++)
+		{
+			const button = pad.buttons[ii];
+
+			this.args['b' + ii] = button.value;
+		}
+	}
+}
